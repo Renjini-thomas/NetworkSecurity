b6c0be6d31e5f036a1a6c781ac3c3ca01a9bbb70
Renjini-thomas <renjini2539thomas@gmail.com>
2025-08-28 22:18:55 +0530

    mlflow done

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -19,6 +19,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import (AdaBoostClassifier,
                               GradientBoostingClassifier,
                               RandomForestClassifier)
+import mlflow
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainingConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -27,6 +28,18 @@ class ModelTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            fl_score = classificationmetric.f1_score
+            precision_score = classificationmetric.precision_score
+            recall_score = classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score",fl_score)  
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")      
+    
         
     def train_model(self,x_train,y_train,x_test,y_test):
         models = {
@@ -44,7 +57,7 @@ class ModelTrainer:
             },
             "Random Forest":{
                 # 'criterion':['gini','entropy','log_loss'],
-                'n_estimators':[8,16,32,64,128,256]
+                'n_estimators':[8,16,32,128,256]
             },
             "Gradient Boosting":{
                 # 'loss':['log_loss', 'exponential'],
@@ -71,8 +84,12 @@ class ModelTrainer:
         y_train_pred = best_model.predict(x_train)
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
 
+        ## track the mlflow
+        self.track_mlflow(best_model,classification_train_metric)
+
         y_test_pred = best_model.predict(x_test)
         classification_test_metric = get_classification_score(y_true=y_test,y_pred=y_test_pred)
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
         model_dir_path = os.path.dirname(self.model_trainer_config.trained_model_file_path)
